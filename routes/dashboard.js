fd8344719ce459f99ab892f1f67658aba0e87a1a
Houng Ting <a0903131602@gmail.com>
2024-07-13 12:39:57 +0800

    相同版型製作新增及轉址

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -16,6 +16,25 @@ router.get('/article/create', function (req, res, next) {
   });
 });
 
+router.get('/article/:id', function (req, res, next) {
+  const id = req.params.id; //取出文章id
+  let categories = {};  // 宣告類別物件來存放firebase資料
+
+  categoriesRef.once('value').then(function(sna){ //從firebase 獲取 categories 資料 => Firebase 獲取文章資料
+    categories = sna.val();
+    return articlesRef.child(id).once('value') //當讀取完成後，會觸發下一個 then 回調函數
+  }).then(function(sna){
+     const article = sna.val();
+     console.log(article)
+     res.render('dashboard/article', {
+      title: 'Express',
+      categories,
+      article
+    });
+  });
+});
+
+// 檔案
 router.get('/archives', function (req, res, next) {
   res.render('dashboard/archives', { title: 'Express' });
 });
@@ -34,10 +53,12 @@ router.get('/categories', function (req, res, next) {
   });
 });
 
+// 註冊
 router.get('/signup', function (req, res, next) {
   res.render('dashboard/signup', { title: 'Express' });
 });
 
+// 新增文章
 router.post('/article/create', function(req, res) {
   const data = {...req.body};
   const articleRef = articlesRef.push();
@@ -48,10 +69,11 @@ router.post('/article/create', function(req, res) {
   console.log('data',data);
 
   articleRef.set(data).then(function(){
-    res.redirect('/dashboard/article/create');
+    res.redirect(`/dashboard/article/${key}`);
   });
 })
 
+// 新增類別
 router.post('/categories/create', function (req, res) {
 
   const data = req.body;
@@ -77,6 +99,7 @@ router.post('/categories/create', function (req, res) {
     })
 });
 
+// 刪除類別
 router.post('/categories/delete/:id', function (req, res) {
   const id = req.params.id;
   categoriesRef.child(id).remove();
