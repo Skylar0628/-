bd56723d285c5899088bd64d4add42e8980db093
user <a0903131602@gmail.com>
2024-07-15 21:19:22 +0800

    將文章呈現在前端台畫面

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var firebaseAdminDB = require('../connections/firebase_admin');
+const categoriesRef = firebaseAdminDB.ref('categories'); 
+const articlesRef = firebaseAdminDB.ref('articles'); 
+const striptags = require('striptags');
+const moment = require('moment');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let categories = {};
+  const articles = [];
+
+  categoriesRef.once('value').then((sna)=>{
+    categories = sna.val();
+    return articlesRef.orderByChild('update_time').once('value')
+  }).then((sna)=>{
+    sna.forEach(item => {
+      if ('public' === item.val().status){
+        articles.push(item.val());
+     }
+    });
+    articles.reverse();
+    console.log(categories,articles)
+    res.render('index', { 
+      title: 'Express',
+      categories,
+      articles,
+      striptags,
+      moment
+    });
+  });
 });
 
-router.get('/post', function(req, res, next) {
+router.get('/post/:id', function(req, res, next) {
+  const id = req.params.id; //取出文章id
+  let categories = {};  // 宣告類別物件來存放firebase資料
+
+  categoriesRef.once('value').then(function(sna){ //從firebase 獲取 categories 資料 => Firebase 獲取文章資料
+    categories = sna.val();
+    return articlesRef.child(id).once('value') //當讀取完成後，會觸發下一個 then 回調函數
+  }).then(function(sna){
+     const article = sna.val();
+     console.log(article)
+     res.render('post', {
+      title: 'Express',
+      categories,
+      article
+    });
+  });
   res.render('post', { title: 'Express' });
 });
 
