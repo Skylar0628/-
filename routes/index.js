09df49da6c1de076d8427e7576cc7225f36ebbeb
user <a0903131602@gmail.com>
2024-07-15 22:23:55 +0800

    前台分頁功能製作 - 後端邏輯    前台分頁功能製作 - 畫面呈現

--- routes/index.js
+++ routes/index.js
@@ -21,14 +21,43 @@ router.get('/', function(req, res, next) {
         articles.push(item.val());
      }
     });
+
+    //分頁
+    const totalResult = articles.length;
+    const perpage = 3;
+    const pageTotal = Math.ceil(totalResult/perpage);
+    let currentPage = Number.parseInt(req.query.page) || 1; // 從查詢參數獲取當前頁碼，默認為第1頁
+
+    if(currentPage > pageTotal){
+      currentPage = pageTotal
+    }
+    const minItem = (currentPage * perpage) - perpage + 1;
+    const macItem = (currentPage * perpage);
+    const data = [];
+    articles.forEach((item, index)=>{
+      const itemNum  = index + 1;
+      if(itemNum >= minItem && itemNum <= macItem){
+        data.push(item);
+      }
+      console.log(data)
+    });
+    const page = {
+      pageTotal,
+      currentPage,
+      hasPre: currentPage > 1,
+      hasNex: currentPage < pageTotal,
+
+    }
+
+    //分頁結束
     articles.reverse();
-    console.log(categories,articles)
     res.render('index', { 
       title: 'Express',
       categories,
-      articles,
+      articles: data,
       striptags,
-      moment
+      moment,
+      page
     });
   });
 });
@@ -42,7 +71,6 @@ router.get('/post/:id', function(req, res, next) {
     return articlesRef.child(id).once('value') //當讀取完成後，會觸發下一個 then 回調函數
   }).then(function(sna){
      const article = sna.val();
-     console.log(article)
      res.render('post', {
       title: 'Express',
       categories,
